21844c86fe2da4bdc10b717f8aca77f45bb3b087
AbdifatahDev <sydictdev@gmail.com>
2026-05-14 15:06:15 +0300

    fixed bugs

--- lib/providers/connectivity_provider.dart
+++ lib/providers/connectivity_provider.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 
 class ConnectivityProvider  extends ChangeNotifier{
   void checkConnectivity() {
-    print("Checking connectivity...");
+    checkInternet();
     // Here you would typically check the actual connectivity status
     // For demonstration, we'll just print a message
+  }
+  void checkInternet() {
+    print("Checking internet connectivity...");
   }
 }
